78db2fde6a1a530ef6bba77104604b3659f29d06
Deviprasad <2414396@cognizant.com>
2025-06-05 10:47:11 +0530

    Addition Commit

---
+++ Addition.java
@@ -0,0 +1,19 @@
+package com.cts.calculator;
+
+import java.util.Scanner;
+
+public class Addition {
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter first number : ");
+		int num1 = sc.nextInt();
+
+		System.out.print("Enter second number : ");
+		int num2 = sc.nextInt();
+
+		System.out.println("Sum of "+num1+" and "+num2+" = "+(num1+num2));
+	}
+}
